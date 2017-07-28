63cbdd723435221ca2522ecb2a208dd91018915c
DavidNu <davidedgardonu121@gmail.com>
2017-07-27 19:14:47 -0600

    Tarea 2

---
+++ cmake-build-debug/CMakeFiles/TareaC2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TareaC2.dir/Evaluador.cpp.obj"
+  "CMakeFiles/TareaC2.dir/Main.cpp.obj"
+  "TareaC2.pdb"
+  "TareaC2.exe"
+  "TareaC2.exe.manifest"
+  "libTareaC2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TareaC2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
